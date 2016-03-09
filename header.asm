1be73414a54bc30d6a47bc07d01968cc7291586d
Nicolette Verlinden <nicole@nicole.moe>
2016-03-08 20:03:17 -0600

    Remove seeks from header.asm, assert header size == 0x50

--- header.asm
+++ header.asm
@@ -1,10 +1,19 @@
 include "fontmap_ascii.inc"
 
+header:
+
+// EXTENDED HEADER
+db "N&" // maker code
+db "TEST" // game code
+db 0,0,0,0,0,0 // reserved
+db $00 // no expansion flash
+db $00 // no expansion ram
+db $00 // not a special version
+db $00 // no custom co-processor
+
 // ROM HEADER
-seek($00ffc0)
-  //123456789012345678901
+//  123456789012345678901
 db "TEST ROM             " // cartridge title
-seek($00ffd5)
 db %00110000 // fast LoROM
 db $00 // ROM, no RAM, no battery
 db $0c // 4 MB rom
@@ -15,17 +24,6 @@ db $00 // version 1.0
 dw $ffff // checksum complement (placeholder)
 dw $0000 // checksum (placeholder)
 
-// EXTENDED HEADER
-seek($00ffb0)
-db "N&" // maker code
-db "TEST" // game code
-db 0,0,0,0,0,0 // reserved
-db $00 // no expansion flash
-db $00 // no expansion ram
-db $00 // not a special version
-db $00 // no custom co-processor
-
-seek($00ffe0)
 dw empty_handler
 dw empty_handler
 dw empty_handler // 65816 COP
@@ -42,3 +40,9 @@ dw empty_handler
 dw empty_handler // 6502 NMI
 dw reset         // 6502 RESET
 dw empty_handler // 6502 IRQ/BRK
+
+header.end:
+
+if header.end - header != $50 {
+    error "Bad header size"
+}
