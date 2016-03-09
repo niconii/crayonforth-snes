fea224209fb35034989041f1b0d7ae8c1ace7382
Nicolette Verlinden <nicole@nicole.moe>
2016-03-08 20:12:17 -0600

    Add seekf macro to avoid memory overlapping

--- main.asm
+++ main.asm
@@ -11,9 +11,12 @@ db 0
 // bank 0
 seek($008000)
 include "start.asm"
-seek($00ffb0)
+seekf($00ffb0)
 include "header.asm"
 
 // bank 1
-seek($018000)
+seekf($018000)
 insert font, "build/gfx/font.2bpp"
+
+// bank 2
+seekf($028000)
