1a0995040c8afd2fd3405b911a61eaf65ed84107
Nicolette Verlinden <nicole@nicole.moe>
2016-03-09 21:52:31 -0600

    Add macros to detect free space and prevent overlaps

--- main.asm
+++ main.asm
@@ -4,19 +4,16 @@ include "macros.inc"
 include "consts.inc"
 include "init.inc"
 
-// ensure rom is padded out to 4 MB
-seek($ffffff)
-db 0
+pad_to($ffffff)
 
-// bank 0
-seek($008000)
+start()
+
+bank(0)
 include "start.asm"
 seekf($00ffb0)
 include "header.asm"
 
-// bank 1
-seekf($018000)
+bank(1)
 insert font, "build/gfx/font.2bpp"
 
-// bank 2
-seekf($028000)
+end()
