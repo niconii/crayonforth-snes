212f54682e8bcf31e44202f9927db07b59170f61
Nicolette Verlinden <nicole@nicole.moe>
2018-09-28 05:39:31 -0500

    Clean up pcx2snes code

--- tools/pcx2snes.rs
+++ tools/pcx2snes.rs
@@ -13,8 +13,8 @@ fn main() {
             let b = args.next().expect("Second input not given");
             let out = args.next().expect("Output not given");
             
-            let mut a_img = Image::from_file(Path::new(&a));
-            let b_img = Image::from_file(Path::new(&b));
+            let mut a_img = PcxImage::from_file(Path::new(&a));
+            let b_img = PcxImage::from_file(Path::new(&b));
             a_img.superimpose(&b_img, 1);
             
             a_img.to_2bpp(Path::new(&out));
@@ -23,7 +23,7 @@ fn main() {
             let a = args.next().expect("First input not given");
             let out = args.next().expect("Output not given");
             
-            let a_img = Image::from_file(Path::new(&a));
+            let a_img = PcxImage::from_file(Path::new(&a));
 
             a_img.to_2bpp(Path::new(&out));
         },
@@ -33,8 +33,8 @@ fn main() {
             let outa = args.next().expect("First output not given");
             let outb = args.next().expect("Second output not given");
             
-            let mut a_img = Image::from_file(Path::new(&a));
-            let b_img = Image::from_file(Path::new(&b));
+            let mut a_img = PcxImage::from_file(Path::new(&a));
+            let b_img = PcxImage::from_file(Path::new(&b));
             a_img.superimpose(&b_img, 1);
             
             a_img.to_font(Path::new(&outa), Path::new(&outb));
@@ -43,15 +43,15 @@ fn main() {
     }
 }
 
-struct Image {
+struct PcxImage {
     width: usize,
     height: usize,
     data: Vec<u8>,
     palette: Vec<u8>
 }
 
-impl Image {
-    fn from_file(filename: &Path) -> Image {
+impl PcxImage {
+    fn from_file(filename: &Path) -> PcxImage {
         let mut f = File::open(&filename).expect("Failed to open file");
 
         let mut header = [0; 128];
@@ -89,7 +89,7 @@ impl Image {
 
         f.read_to_end(&mut palette).expect("Failed to read palette");
 
-        Image {
+        PcxImage {
             width: width,
             height: height,
             data: data,
@@ -97,7 +97,7 @@ impl Image {
         }
     }
 
-    fn to_2bpp(&self, filename: &Path) {
+    fn read_plane(&self, stride: usize, offset: usize) -> Vec<u8> {
         let mut output = vec![];
 
         for y in 0..(self.height/8) {
@@ -107,7 +107,9 @@ impl Image {
                     let mut row1 = 0u8;
 
                     for tx in 0..8 {
-                        let pixel = self.data[(y*8 + ty)*self.width + x*8 + tx];
+                        let pixel = self.data[
+                            (8*y + ty)*self.width + 8*x + stride*tx + offset
+                        ];
                         row0 |= (pixel & 1) << (7-tx);
                         row1 |= ((pixel & 2) >> 1) << (7-tx);
                     }
@@ -118,62 +120,19 @@ impl Image {
             }
         }
 
-        let mut out = File::create(filename).expect("Failed to create output file");
+        output
+    }
 
-        out.write_all(&output).expect("Failed to write output");
+    fn to_2bpp(&self, filename: &Path) {
+        create_file(filename, self.read_plane(1, 0));
     }
     
     fn to_font(&self, evens_filename: &Path, odds_filename: &Path) {
-        let mut output = vec![];
-
-        for y in 0..(self.height/8) {
-            for x in 0..(self.width/8) {
-                for ty in 0..8 {
-                    let mut row0 = 0u8;
-                    let mut row1 = 0u8;
-
-                    for tx in 0..4 {
-                        let pixel = self.data[(y*8 + ty)*self.width + x*8 + tx*2];
-                        row0 |= (pixel & 1) << (7-tx);
-                        row1 |= ((pixel & 2) >> 1) << (7-tx);
-                    }
-
-                    output.push(row0);
-                    output.push(row1);
-                }
-            }
-        }
-        
-        let mut out = File::create(evens_filename).expect("Failed to create output file");
-
-        out.write_all(&output).expect("Failed to write output");
-
-        let mut output = vec![];
-        
-        for y in 0..(self.height/8) {
-            for x in 0..(self.width/8) {
-                for ty in 0..8 {
-                    let mut row0 = 0u8;
-                    let mut row1 = 0u8;
-
-                    for tx in 0..4 {
-                        let pixel = self.data[(y*8 + ty)*self.width + x*8 + tx*2 + 1];
-                        row0 |= (pixel & 1) << (7-tx);
-                        row1 |= ((pixel & 2) >> 1) << (7-tx);
-                    }
-
-                    output.push(row0);
-                    output.push(row1);
-                }
-            }
-        }
-
-        let mut out = File::create(odds_filename).expect("Failed to create output file");
-
-        out.write_all(&output).expect("Failed to write output");
+        create_file(evens_filename, self.read_plane(2, 0));
+        create_file(odds_filename, self.read_plane(2, 1));
     }
     
-    fn superimpose(&mut self, image: &Image, shift: usize) {
+    fn superimpose(&mut self, image: &PcxImage, shift: usize) {
         assert_eq!(self.width, image.width);
         assert_eq!(self.height, image.height);
 
@@ -181,4 +140,9 @@ impl Image {
             *byte |= image.data[i] << shift;
         }
     }
+}
+
+fn create_file(filename: &Path, data: Vec<u8>) {
+    let mut out = File::create(filename).expect("Failed to create output file");
+    out.write_all(&data).expect("Failed to write output");
 }
